use3168/Adhell3

Mobile data restriction
After disabling mobile data for some apps, Adhell needs to be turn off and then turn on.

app/src/main/java/com/fiendfyre/AdHell2/MainActivity.java
@@ -23,6 +23,7 @@ import com.fiendfyre.AdHell2.dialogfragment.NoInternetConnectionDialogFragment;
 import com.fiendfyre.AdHell2.fragments.AdhellNotSupportedFragment;
 import com.fiendfyre.AdHell2.fragments.AdhellPermissionInfoFragment;
 import com.fiendfyre.AdHell2.fragments.BlockerFragment;
+import com.fiendfyre.AdHell2.fragments.MobileRestricterFragment;
 import com.fiendfyre.AdHell2.fragments.PackageDisablerFragment;
 import com.fiendfyre.AdHell2.service.BlockedDomainService;
 import com.fiendfyre.AdHell2.utils.AdhellAppIntegrity;
@@ -168,6 +169,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.packageDisablerTab:
                 replacing = new PackageDisablerFragment();
+                break;
+            case R.id.mobileRestricterTab:
+                replacing = new MobileRestricterFragment();
                 break;
             case R.id.appPermissionsTab:
                 replacing = new AdhellPermissionInfoFragment();

app/src/main/java/com/fiendfyre/AdHell2/dagger/component/AppComponent.java
@@ -10,6 +10,7 @@ import com.fiendfyre.AdHell2.dagger.module.NetworkModule;
 import com.fiendfyre.AdHell2.dagger.scope.AdhellApplicationScope;
 import com.fiendfyre.AdHell2.fragments.BlockedUrlSettingFragment;
 import com.fiendfyre.AdHell2.fragments.BlockerFragment;
+import com.fiendfyre.AdHell2.fragments.MobileRestricterFragment;
 import com.fiendfyre.AdHell2.fragments.PackageDisablerFragment;
 import com.fiendfyre.AdHell2.service.BlockedDomainService;
 import com.fiendfyre.AdHell2.utils.AdhellAppIntegrity;
@@ -34,6 +35,8 @@ public interface AppComponent {
     void inject(BlockedUrlSettingFragment blockedUrlSettingFragment);
 
     void inject(PackageDisablerFragment packageDisablerFragment);
+
+    void inject(MobileRestricterFragment mobileRestricterFragment);
 
     void inject(AdhellWhitelistAppsViewModel adhellWhitelistAppsViewModel);
 

reunicorn/SABS

app/src/main/java/com/layoutxml/sabs/MainActivity.java
@@ -41,6 +41,7 @@ import com.layoutxml.sabs.fragments.AppListFragment;
 import com.layoutxml.sabs.fragments.BlockCustomUrlFragment;
 import com.layoutxml.sabs.fragments.BlockedUrlSettingFragment;
 import com.layoutxml.sabs.fragments.BlockerFragment;
+import com.layoutxml.sabs.fragments.MobileRestricterFragment;
 import com.layoutxml.sabs.fragments.CustomBlockUrlProviderFragment;
 import com.layoutxml.sabs.fragments.AboutFragment;
 import com.layoutxml.sabs.fragments.DonateFragment;
@@ -235,6 +236,9 @@ public class MainActivity extends AppCompatActivity {
                                 break;
                             case R.id.packageDisablerTab:
                                 replacing = new PackageDisablerFragment();
+                                break;
+                            case R.id.mobileRestricterTab:
+                                replacing = new MobileRestricterFragment();
                                 break;
                             case R.id.appPermissionsTab:
                                 replacing = new AdhellPermissionInfoFragment();

app/src/main/java/com/layoutxml/sabs/dagger/component/AppComponent.java
@@ -11,6 +11,7 @@ import com.layoutxml.sabs.dagger.module.NetworkModule;
 import com.layoutxml.sabs.dagger.scope.AdhellApplicationScope;
 import com.layoutxml.sabs.fragments.BlockedUrlSettingFragment;
 import com.layoutxml.sabs.fragments.BlockerFragment;
+import com.layoutxml.sabs.fragments.MobileRestricterFragment;
 import com.layoutxml.sabs.fragments.PackageDisablerFragment;
 import com.layoutxml.sabs.service.BlockedDomainService;
 import com.layoutxml.sabs.utils.AdhellAppIntegrity;
@@ -37,6 +38,8 @@ public interface AppComponent {
     void inject(BlockedUrlSettingFragment blockedUrlSettingFragment);
 
     void inject(PackageDisablerFragment packageDisablerFragment);
+
+    void inject(MobileRestricterFragment mobileRestricterFragment);
 
     void inject(AdhellWhitelistAppsViewModel adhellWhitelistAppsViewModel);
 